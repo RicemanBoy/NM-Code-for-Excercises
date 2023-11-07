a06b876d647b96ffdf917923790fcfcfd07600a8
RicemanBoy <dustin.seboldt@t-online.de>
2023-11-07 18:11:17 +0100

    modified:   Blatt 3/excercise4.ipynb 	modified:   __pycache__/shortcuts.cpython-311.pyc 	modified:   shortcuts.py

--- shortcuts.py
+++ shortcuts.py
@@ -127,15 +127,28 @@ def tridiag_LU_decomp(a,b,c,vecb):
     
     return L,R,x
 
-def matrix_dot(X,Y):
-    row = np.shape(X)[0]
-    coloumn = np.shape(Y)[1]
-    result = np.zeros(row,coloumn)
-    for i in range(len(X)):
-        for j in range(len(Y[0])):
-            for k in range(len(Y)):
-                result[i][j] += X[i][k] * Y[k][j]
-    return result
+# def matrix_dot(X,Y):
+#     row = np.shape(X)[0]
+
+#     coloumn = np.shape(Y)[1]
+#     result = np.zeros(row,coloumn)
+#     for i in range(len(X)):
+#         for j in range(len(Y[0])):
+#             for k in range(len(Y)):
+#                 result[i][j] += X[i][k] * Y[k][j]
+#     return result
+
+def multiply_matrix(A,B):
+  global C
+  if  A.shape[1] == B.shape[0]:
+    C = np.zeros((A.shape[0],B.shape[1]),dtype = int)
+    for row in range(rows): 
+        for col in range(cols):
+            for elt in range(len(B)):
+              C[row, col] += A[row, elt] * B[elt, col]
+    return C
+  else:
+    return "Sorry, cannot multiply A and B."
 
 ##################################################################################################################################################################
     
